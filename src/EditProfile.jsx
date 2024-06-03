38df706fd0f98190bc604143bca577f786e02bf3
kausikrstnj <kausikrstnj@gmail.com>
2024-06-03 16:03:02 +0530

    EditProfile validation

--- src/EditProfile.jsx
+++ src/EditProfile.jsx
@@ -9,17 +9,31 @@ function EditProfile() {
     const navigate = useNavigate();
     const [userData, setUserData] = useState('');
     const { userId } = useParams();
+    const [formErrors, setFormErrors] = useState({});
 
     const editAccount = async (userId) => {
         event.preventDefault();
+        const errors = {};
         const nameInput = document.getElementById('name');
         const emailInput = document.getElementById('email');
         const phoneInput = document.getElementById('phn');
-        if (nameInput.value === '' || emailInput.value === '' || phoneInput.value === '') {
-            alert('Please fill in all fields.');
-            return;
+
+        console.log('nameInput:', nameInput);
+        console.log('emailInput:', emailInput);
+        console.log('phoneInput:', phoneInput);
+
+        if (!nameInput.value) errors.nameInput = "Name is required.";
+        if (!emailInput.value) errors.emailInput = "Email is required.";
+        if (!phoneInput.value) errors.phoneInput = "Phone is required.";
+
+        console.log('errors.length:', errors.length);
+
+        setFormErrors(errors);
+
+        if (Object.keys(errors).length > 0) {
+            return alert('Please fill all the required fields.');
         }
-        document.getElementById('loader').style.display = 'block';
+
         const data = {
             name: nameInput.value,
             email: emailInput.value,
@@ -54,14 +68,17 @@ function EditProfile() {
                     <div className="form-group mt-3">
                         <label htmlFor="name" className='form-label'>Name</label>
                         <input className='form-control' required placeholder="Enter your name" name="name" id="name" type="text" />
+                        {formErrors.nameInput && <div className="text-danger">{formErrors.nameInput}</div>}
                     </div>
                     <div className="form-group mt-3">
                         <label htmlFor="email" className='form-label'>Email</label>
                         <input className='form-control' required placeholder="Enter your email" name="email" id="email" type="text" />
+                        {formErrors.emailInput && <div className="text-danger">{formErrors.emailInput}</div>}
                     </div>
                     <div className="form-group mt-3">
                         <label htmlFor="phn" className='form-label'>Phone</label>
                         <input className='form-control' required name="phn" placeholder="Enter phone" id="phn" type="text" maxLength={10} />
+                        {formErrors.phoneInput && <div className="text-danger">{formErrors.phoneInput}</div>}
                     </div>
 
                     <button type="submit" className="form-submit-btn mt-5" onClick={() => editAccount(userId)}>Save changes</button>
