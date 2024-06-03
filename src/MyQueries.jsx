ec480f539f338daea700a0afbe2ccb9fa6f33de5
kausikrstnj <kausikrstnj@gmail.com>
2024-06-03 19:44:00 +0530

    Filter

--- src/MyQueries.jsx
+++ src/MyQueries.jsx
@@ -258,12 +258,7 @@ function MyQueries() {
                                 <path d="M7.667 12.667A5.333 5.333 0 107.667 2a5.333 5.333 0 000 10.667zM14.334 14l-2.9-2.9" stroke="currentColor" strokeWidth="1.333" strokeLinecap="round" strokeLinejoin="round"></path>
                             </svg>
                         </button>
-                        <input className="input" placeholder="EX : QNo/Title" type="text" id='filterInput' onChange={handleFilterChange} />
-                        <button className="reset" type="reset" style={{ color: 'black' }} onClick={() => setFilterInput('')}>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path>
-                            </svg>
-                        </button>
+                        <input style={{ fontFamily: 'sans-serif', fontWeight: '100' }} className="input" placeholder="EX : QNo/Title" type="text" id='filterInput' onChange={handleFilterChange} />
                     </form>
 
                     {loading ? (
